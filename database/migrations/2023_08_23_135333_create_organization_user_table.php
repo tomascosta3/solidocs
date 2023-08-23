c9c47951764d2ed97b27aabb1dc2132430c16e7c
Tomás Costa <tomasc98@live.com>
2023-08-23 10:56:34 -0300

    Organization User Migration: Created

---
+++ database/migrations/2023_08_23_135333_create_organization_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('organization_user', function (Blueprint $table) {
+            
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->unsignedBigInteger('organization_id');
+            $table->foreign('organization_id')->references('id')->on('organizations')->onDelete('cascade');
+
+            $table->unsignedBigInteger('access_level')->default(1);
+
+            $table->boolean('active', true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('organization_user');
+    }
+};
