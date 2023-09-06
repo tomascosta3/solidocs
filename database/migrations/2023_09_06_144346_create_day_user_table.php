8923d009530390a087dee114cbd196257a07cb04
Tomás Costa <tomasc98@live.com>
2023-09-06 15:11:05 -0300

    feat(day user model): added

---
+++ database/migrations/2023_09_06_144346_create_day_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('day_user', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->unsignedBigInteger('day_id');
+            $table->foreign('day_id')->references('id')->on('days')->onDelete('cascade');
+
+            $table->integer('days_available')->nullable();
+
+            $table->boolean('active')->default(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('day_user');
+    }
+};
