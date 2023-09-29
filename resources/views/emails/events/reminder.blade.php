4479804e9f83f3b72e57ef9fe94f9a9209017bde
Tomás Costa <tomasc98@live.com>
2023-09-29 15:18:57 -0300

    style(event reminder email): style changed

--- resources/views/emails/events/reminder.blade.php
+++ resources/views/emails/events/reminder.blade.php
@@ -1,7 +1,7 @@
 <x-mail::message>
 # ¡No te olvides tu próximo/a {{ $event->event_type->name }}!
 
-El evento {{ $event->title }} está programado para comenzar el {{ $event->start }}.
+El evento <strong>{{ $event->title }}</strong> está programado para comenzar el {{ $event->start }}.
 
 <x-mail::button :url="$calendars_link">
 Más información del evento.
